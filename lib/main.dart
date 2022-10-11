39c668bff13ed9c4878a9d7a4d7ba77450078123
BilalAhmed-1998 <69021317+BilalAhmed-1998@users.noreply.github.com>
2022-10-11 20:49:09 +0500

    Oct 2022
    Changings of some packages and upgrade build gradle version

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
 import 'package:specialite_foodapp/routes.dart';
 import 'package:specialite_foodapp/screens/splashScreen.dart';
